35f34db2b5e4e6a999279f66d88f61151df21afc
Mubtasim <mubtasimf443@gmail.com>
2024-09-06 22:14:29 +0600

    Alhamdullilah , Thanks for All

---
+++ sv/.js
@@ -0,0 +1,44 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  ﷺ  
+InshaAllah, By his marcy I will Gain Success 
+*/
+
+
+import express from 'express'
+import {fileURLToPath} from 'url'
+import path from "path";
+
+
+const app = express()
+const __filename = fileURLToPath(import.meta.url);
+let dirName = path.dirname(__filename)
+app.set('view engine','hbs')
+
+
+app.get('/css/:name', (req, res) => {
+    try {
+       return res.status(200).sendFile(path.resolve(dirName, './assets/css/' + req.params.name))
+    } catch (error) {
+        log(error)
+        return res.sendStatus(304)
+    }
+})
+
+
+
+app.get('/img/:name', (req, res) => {
+    try {
+       return res.status(200).sendFile(path.resolve(dirName, './assets/img/' + req.params.name))
+    } catch (error) {
+        log(error)
+        return res.sendStatus(304)
+    }
+})
+
+
+app.get('*',(req, res) => res.render('home') )
+
+
+
+app.listen(4000)
+
